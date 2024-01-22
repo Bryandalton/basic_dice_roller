dca1f50167288f2572add1bdc466d48f07acfe58
unknown <bryantdalton19@gmail.com>
2024-01-22 12:55:48 -0500

    initial commit

---
+++ main.py
@@ -0,0 +1,38 @@
+import random
+
+print("welcome to dice roller basic!")
+print("----------------------------")
+
+# print("how many six sided dice would you like to roll?")
+
+#validate input
+while True:
+    try:
+        faces= int(input('How any sides would you like your die to have? (max: 100) '))
+        if (faces > 0 and faces <= 100):
+            pass
+        else:print("invalid input, please try again.")
+        num_pick= int(input('How many dice would you like to roll? (max:10) '))
+        if (num_pick > 0 and num_pick <= 10):
+            break
+        else:print("invalid input, please try again.")
+    except:
+        print("please provide an integer.")
+
+def rollDice(amountofdice, faces):
+    totalsum= 0
+    #stores values for highest
+    hidden_list = []
+    possiblefaces = range(1 ,faces + 1)
+    for die in range(amountofdice):
+        roll = random.choice(possiblefaces)
+        hidden_list.append(roll)
+        print('Die', die + 1,": ", roll)
+        totalsum += roll
+    average = totalsum / amountofdice
+    highest = max(hidden_list)
+    print('Total: ', totalsum)
+    print('Average: ', average)
+    print('Highest: ', highest)
+
+rollDice(num_pick, faces)
